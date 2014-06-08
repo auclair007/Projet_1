4bcd6c52b70e924dbab206c7e8a884d247d78ab0
auclair007 <pa.auclair@gmail.com>
2014-06-08 20:27:05 +0200

    commit global

--- Gmap_1/src/www/pascal/MainActivity.java
+++ Gmap_1/src/www/pascal/MainActivity.java
@@ -1,5 +1,31 @@
-package www.pascal.gmap_1;
+package www.pascal;
 
+import android.os.Bundle;
+import android.app.Activity;
+//import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+//	private GoogleMap mMap;
+//	mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	
+
+
+	/*	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.map, menu);
+		return true;
+	}
+*/
+}
+
+/*
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +36,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import www.pascal.R;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -46,9 +73,9 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
+    
+     // A placeholder fragment containing a simple view.
+     
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
@@ -63,3 +90,4 @@ public class MainActivity extends ActionBarActivity {
     }
 
 }
+*/
